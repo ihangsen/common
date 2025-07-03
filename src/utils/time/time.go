74cd1e0dd406c959f2bce057b21022205027445c
wuruiyao <xiaow404@qq.com>
2025-07-03 17:03:29 +0800

    添加时间处理方法

--- src/utils/time/time.go
+++ src/utils/time/time.go
@@ -32,7 +32,7 @@ func StartWeek(now time.Time) time.Time {
 	return time.Date(monday.Year(), monday.Month(), monday.Day(), 0, 0, 0, 0, monday.Location())
 }
 
-// EndWeek 本周结束时间
+// EndWeek 本周结束时间(最后一天的起始时间)
 func EndWeek(now time.Time) time.Time {
 	weekday := int(now.Weekday())
 	daysToSunday := 7 - weekday
@@ -62,4 +62,30 @@ func NextWeek(now time.Time) time.Time {
 	daysToNextMonday := 8 - weekday
 	nextMonday := now.AddDate(0, 0, daysToNextMonday)
 	return time.Date(nextMonday.Year(), nextMonday.Month(), nextMonday.Day(), 0, 0, 0, 0, nextMonday.Location())
+}
+
+// StartMonth 本月起始时间
+func StartMonth(now time.Time) time.Time {
+	return time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, now.Location())
+}
+
+// EndMonth 本月结束时间(最后一天的起始时间)
+func EndMonth(now time.Time) time.Time {
+	// 获取下个月的第一天，然后减去一纳秒，得到本月最后一天
+	nextMonth := now.AddDate(0, 1, 0)
+	firstDayOfNextMonth := time.Date(nextMonth.Year(), nextMonth.Month(), 1, 0, 0, 0, 0, nextMonth.Location())
+	return firstDayOfNextMonth.AddDate(0, 0, -1)
+}
+
+// StartYear 本年起始时间
+func StartYear(now time.Time) time.Time {
+	return time.Date(now.Year(), time.January, 1, 0, 0, 0, 0, now.Location())
+}
+
+// EndYear 本年结束时间(最后一天的起始时间)
+func EndYear(now time.Time) time.Time {
+	// 获取下一年的第一天，然后减去一天，得到本年最后一天
+	nextYear := now.AddDate(1, 0, 0)
+	firstDayOfNextYear := time.Date(nextYear.Year(), time.January, 1, 0, 0, 0, 0, nextYear.Location())
+	return firstDayOfNextYear.AddDate(0, 0, -1)
 }
