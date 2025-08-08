d0eff45297f1b7021a337f03bac78b5df22ce703
xs <your_email@example.com>
2025-08-08 19:07:10 +0800

    strs 占位符

--- src/utils/strs/str.go
+++ src/utils/strs/str.go
@@ -1,9 +1,23 @@
 package strs
 
 import (
+	"fmt"
+	"regexp"
 	"strings"
 )
 
+var printfReg = regexp.MustCompile(`\{(\w+)}`)
+
 func Join(ss ...string) string {
 	return strings.Join(ss, "")
+}
+
+func Printf(format string, params map[string]any) string {
+	return printfReg.ReplaceAllStringFunc(format, func(str string) string {
+		key := printfReg.FindStringSubmatch(str)[1]
+		if val, ok := params[key]; ok {
+			return fmt.Sprint(val)
+		}
+		return str
+	})
 }
